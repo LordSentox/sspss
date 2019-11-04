b7d139d59a7c5d967f1b5229ec3f9d3d7099d41e
Arne Dußin <arne.dussin@live.de>
2019-11-04 16:28:58 +0100

    Add test test for crypt string

--- src/crypt_string.rs
+++ src/crypt_string.rs
@@ -4,3 +4,11 @@ pub enum CryptString {
     Encrypted(Vec<u8>),
     Decrypted(String),
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn rarr() {
+        let crypt_string = CryptString::Decrypted("arrrrgh");
+    }
+}
